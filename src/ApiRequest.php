439158e88dba16bfe615eab757dcb30fdc75aefd
Chinthaka Abeykoon <pathum4u@gmail.com>
2023-03-22 11:29:05 +0530

    done

--- src/ApiRequest.php
+++ src/ApiRequest.php
@@ -5,64 +5,67 @@ namespace Pathum4u\ApiRequest;
 use GuzzleHttp\Client;
 use GuzzleHttp\Psr7\Utils;
 use GuzzleHttp\Psr7\Request;
+use Illuminate\Support\Facades\Http;
 use Pathum4u\ApiRequest\ApiResponse;
 use GuzzleHttp\Exception\ClientException;
 use GuzzleHttp\Exception\ServerException;
 use GuzzleHttp\Exception\RequestException;
+use Illuminate\Http\Client\PendingRequest;
 use GuzzleHttp\Exception\BadResponseException;
 
 
-class ApiRequest extends ApiResponse
+class ApiRequest extends Http
 {
-    /**
-     * @var string
-     */
-    protected $baseUri;
 
-    /**
-     * @var string
-     */
-    protected $secret;
+    // /**
+    //  * @var string
+    //  */
+    // protected $baseUri;
+
+    // /**
+    //  * @var string
+    //  */
+    // protected $secret;
 
     /**
      * @var string
      */
     protected $services = [];
 
-    /**
-     * @var string
-     */
-    protected $method;
+    // /**
+    //  * @var string
+    //  */
+    // protected $method;
 
-    /**
-     * @var string
-     */
-    protected $url;
+    // /**
+    //  * @var string
+    //  */
+    // protected $url;
 
-    /**
-     * @var array
-     */
-    protected $params = [];
+    // /**
+    //  * @var array
+    //  */
+    // protected $params = [];
 
-    /**
-     * @var array
-     */
-    protected $headers = [];
+    // /**
+    //  * @var array
+    //  */
+    // protected $headers = [];
 
-    /**
-     * @var bool
-     */
-    protected $debug = false;
+    // /**
+    //  * @var bool
+    //  */
+    // protected $debug = false;
 
-    /**
-     * @var array
-     */
-    protected $multipart = [];
+    // /**
+    //  * @var array
+    //  */
+    // protected $multipart = [];
 
-    /**
-     * @var array
-     */
-    protected $data = ['multipart' => []];
+    // /**
+    //  * @var array
+    //  */
+    // protected $data = ['multipart' => []];
 
     /**
      * constructor
@@ -73,31 +76,31 @@ class ApiRequest extends ApiResponse
         $this->services = config('services');
     }
 
-    /**
-     * Base url provider
-     *
-     */
-    public function base_url($url)
-    {
-        $this->baseUri = $url;
+    // /**
+    //  * Base url provider
+    //  *
+    //  */
+    // public function base_url($url)
+    // {
+    //     $this->baseUri = $url;
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
     /**
      * Service providers
      *
      */
-    public function service($service)
-    {
-        //
-        if ($service = $this->checkService($service)) {
-            $this->baseUri = $service['base_uri'];
-            $this->secret = $service['secret'];
-        }
+    // public function service($service)
+    // {
+    //     //
+    //     if ($service = $this->checkService($service)) {
+    //         $this->baseUri = $service['base_uri'];
+    //         $this->secret = $service['secret'];
+    //     }
 
-        return $this;
-    }
+    //     return $this;
+    // }
 
     /**
      * Check Service
@@ -114,257 +117,286 @@ class ApiRequest extends ApiResponse
         return dd('service not available');
     }
 
+    // /**
+    //  * method
+    //  *
+    //  */
+    // public function method($method)
+    // {
+    //     //
+    //     $this->method = $method;
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * Get method
+    //  *
+    //  */
+    // public function get($url)
+    // {
+    //     //
+    //     $this->method = 'get';
+    //     $this->url($url);
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * Post method
+    //  *
+    //  */
+    // public function post($url)
+    // {
+    //     //
+    //     $this->method = 'post';
+    //     $this->url($url);
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * Url
+    //  *
+    //  */
+    // public function url($url)
+    // {
+    //     //
+    //     if ($url) {
+    //         $this->url = $url;
+    //     } else {
+    //         return dd('url not valied');
+    //     }
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * Url
+    //  *
+    //  */
+    // public function params($params)
+    // {
+    //     //
+    //     $this->data['multipart'] = array_merge($this->data['multipart'], $this->set_params($params));
+    //     // dd($this->data);
+    //     return $this;
+    // }
+
+    // /**
+    //  * Set multipart data
+    //  *
+    //  */
+    // public function set_params($params)
+    // {
+    //     $data = [];
+
+    //     foreach ($params as $key => $value) {
+    //         $data[] = ['name' => $key, 'contents' => $value];
+    //     }
+
+    //     return $data;
+    // }
+
+    // /**
+    //  * User
+    //  *
+    //  */
+    // public function user($user = null)
+    // {
+    //     //
+    //     // $this->params = array_merge($this->params, ['user' => $user]);
+    //     $this->data['multipart'][] = ['name' => 'user', 'contents' => $user];
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * files/multipart
+    //  *
+    //  */
+    // public function attach($files)
+    // {
+    //     //
+    //     $data = [];
+
+    //     if (is_array($files)) {
+    //         foreach ($files as $file) {
+    //             $data[] = array_filter([
+    //                 'contents' =>  Utils::tryFopen($file, 'r'),
+    //                 'filename' => $file->getClientOriginalName(),
+    //             ]);
+    //         }
+
+    //         $this->data['multipart'][] = ['name' => 'file', 'contents' => $data];
+    //         return $this;
+    //     }
+
+
+    //     $data = array_filter([
+    //         'name' => 'file',
+    //         'contents' =>   Utils::tryFopen($files, 'r'), //$files,
+    //         'filename' => $files->getClientOriginalName(),
+    //     ]);
+
+    //     $this->data['multipart'][] = $data;
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * Debug
+    //  *
+    //  */
+    // public function debug($debug = true)
+    // {
+    //     $this->debug = $debug;
+    // }
+
+    // /**
+    //  * Debug
+    //  *
+    //  */
+    // public function json($params)
+    // {
+    //     $this->data = ['json'=> $params];
+    // }
+
+    // /**
+    //  * Headers
+    //  *
+    //  */
+    // function headers($headers)
+    // {
+    //     //
+    //     if (is_array($headers)) {
+    //         array_merge($headers, $this->headers);
+    //     } else {
+    //         return dd('Headers not in array');
+    //     }
+
+    //     return $this;
+    // }
+
+    // /**
+    //  * @param       $method
+    //  * @param       $requestUrl
+    //  * @param array $formParams
+    //  * @param array $headers
+    //  *
+    //  * @return string
+    //  * @throws \GuzzleHttp\Exception\GuzzleException
+    //  */
+    // public function request($service, $method, $url, $user = null,  $params = [], $files = [], $headers = [], $debug = false)
+    // {
+    //     //
+    //     $this->service($service);
+    //     $this->url($url);
+    //     $this->method($method);
+    //     $this->params($params);
+    //     $this->user($user);
+    //     $this->headers($headers);
+    //     $this->debug($debug);
+    //     $this->attach($files);
+
+    //     return $this->send();
+    // }
+
+    // /**
+    //  * Create request
+    //  *
+    //  * @response $request
+    //  */
+    // public function set_request()
+    // {
+    //     //
+    //     return  new Request(
+    //         $this->method,
+    //         $this->url,
+    //         [
+    //             'multipart' => $this->params
+    //         ]
+    //     );
+    // }
+
+    // /**
+    //  * Send request
+    //  *
+    //  *
+    //  */
+    // public function send()
+    // {
+    //     //
+    //     $client = new Client([
+    //         'timeout'  => 10.0,
+    //         'base_uri' => $this->baseUri,
+    //         'headers' => [
+    //             'Secret' => $this->secret,
+    //             'Accept'     => 'application/multipart',
+    //             'Content-Type'      => 'application/multipart'
+    //         ]
+    //     ]);
+
+    //     if ($this->debug) {
+
+    //         $response =  $client->request(
+    //             $this->method,
+    //             $this->url,
+    //             $this->data
+
+    //             // [
+    //             //     'multipart' => $this->params,
+    //             //     'multipart' => $this->multipart
+    //             // ]
+    //         );
+
+    //         return $response->getBody()->getContents();
+    //     } else {
+    //         try {
+    //             $response =  $client->request(
+    //                 $this->method,
+    //                 $this->url,
+    //                 $this->data
+
+    //                 // [
+    //                 //     'multipart' => $this->params,
+    //                 //     'multipart' => $this->multipart
+    //                 // ]
+    //             );
+
+    //             return $this->successResponse($response->getStatusCode(), $response->getBody()->getContents());
+    //         } catch (ClientException $e) {
+
+    //             return $this->errorResponse($e);
+    //         } catch (ServerException $e) {
+
+    //             return $this->errorResponse($e);
+    //         } catch (BadResponseException $e) {
+
+    //             return $this->errorResponse($e);
+    //         } catch (RequestException $e) {
+    //             return $this->errorRequest($this);
+    //         }
+    //     }
+    // }
+
+
+    // public function services()
+    // {
+    //     return $this->baseUrl('https://github.com');
+    // }
+
     /**
-     * method
+     * Dump the request before sending and end the script.
      *
+     * @return $this
      */
-    public function method($method)
+    public function Service($service)
     {
-        //
-        $this->method = $method;
-
-        return $this;
-    }
-
-    /**
-     * Get method
-     *
-     */
-    public function get($url)
-    {
-        //
-        $this->method = 'get';
-        $this->url($url);
-
-        return $this;
-    }
-
-    /**
-     * Post method
-     *
-     */
-    public function post($url)
-    {
-        //
-        $this->method = 'post';
-        $this->url($url);
-
-        return $this;
-    }
-
-    /**
-     * Url
-     *
-     */
-    public function url($url)
-    {
-        //
-        if ($url) {
-            $this->url = $url;
-        } else {
-            return dd('url not valied');
-        }
-
-        return $this;
-    }
-
-    /**
-     * Url
-     *
-     */
-    public function params($params)
-    {
-        //
-        $this->data['multipart'] = array_merge($this->data['multipart'], $this->set_params($params));
-        // dd($this->data);
-        return $this;
-    }
-
-    /**
-     * Set multipart data
-     *
-     */
-    public function set_params($params)
-    {
-        $data = [];
-
-        foreach ($params as $key => $value) {
-            $data[] = ['name' => $key, 'contents' => $value];
-        }
-
-        return $data;
-    }
-
-    /**
-     * User
-     *
-     */
-    public function user($user = null)
-    {
-        //
-        // $this->params = array_merge($this->params, ['user' => $user]);
-        $this->data['multipart'][] = ['name' => 'user', 'contents' => $user];
-
-        return $this;
-    }
-
-    /**
-     * files/multipart
-     *
-     */
-    public function attach($files)
-    {
-        //
-        $data = [];
-
-        if (is_array($files)) {
-            foreach ($files as $file) {
-                $data[] = array_filter([
-                    'contents' =>  Utils::tryFopen($file, 'r'),
-                    'filename' => $file->getClientOriginalName(),
-                ]);
-            }
-
-            $this->data['multipart'][] = ['name' => 'file', 'contents' => $data];
-            return $this;
-        }
-
-
-        $data = array_filter([
-            'name' => 'file',
-            'contents' =>   Utils::tryFopen($files, 'r'), //$files,
-            'filename' => $files->getClientOriginalName(),
-        ]);
-
-        $this->data['multipart'][] = $data;
-
-        return $this;
-    }
-
-    /**
-     * Debug
-     *
-     */
-    public function debug($debug = true)
-    {
-        $this->debug = $debug;
-    }
-
-    /**
-     * Headers
-     *
-     */
-    function headers($headers)
-    {
-        //
-        if (is_array($headers)) {
-            array_merge($headers, $this->headers);
-        } else {
-            return dd('Headers not in array');
-        }
-
-        return $this;
-    }
-
-    /**
-     * @param       $method
-     * @param       $requestUrl
-     * @param array $formParams
-     * @param array $headers
-     *
-     * @return string
-     * @throws \GuzzleHttp\Exception\GuzzleException
-     */
-    public function request($service, $method, $url, $user = null,  $params = [], $files = [], $headers = [], $debug = false)
-    {
-        //
-        $this->service($service);
-        $this->url($url);
-        $this->method($method);
-        $this->params($params);
-        $this->user($user);
-        $this->headers($headers);
-        $this->debug($debug);
-        $this->attach($files);
-
-        return $this->send();
-    }
-
-    /**
-     * Create request
-     *
-     * @response $request
-     */
-    public function set_request()
-    {
-        //
-        return  new Request(
-            $this->method,
-            $this->url,
-            [
-                'multipart' => $this->params
-            ]
-        );
-    }
-
-    /**
-     * Send request
-     *
-     *
-     */
-    public function send()
-    {
-        //
-        $client = new Client([
-            'timeout'  => 10.0,
-            'base_uri' => $this->baseUri,
-            'headers' => [
-                'Secret' => $this->secret,
-                'Accept'     => 'application/multipart',
-                'Content-Type'      => 'application/multipart'
-            ]
-        ]);
-
-        if ($this->debug) {
-
-            $response =  $client->request(
-                $this->method,
-                $this->url,
-                $this->data
-
-                // [
-                //     'multipart' => $this->params,
-                //     'multipart' => $this->multipart
-                // ]
-            );
-
-            return $response->getBody()->getContents();
-        } else {
-            try {
-                $response =  $client->request(
-                    $this->method,
-                    $this->url,
-                    $this->data
-
-                    // [
-                    //     'multipart' => $this->params,
-                    //     'multipart' => $this->multipart
-                    // ]
-                );
-
-                return $this->successResponse($response->getStatusCode(), $response->getBody()->getContents());
-            } catch (ClientException $e) {
-
-                return $this->errorResponse($e);
-            } catch (ServerException $e) {
-
-                return $this->errorResponse($e);
-            } catch (BadResponseException $e) {
-
-                return $this->errorResponse($e);
-            } catch (RequestException $e) {
-                return $this->errorRequest($this);
-            }
+        if ($service = $this->checkService($service)) {
+            return Http::withHeaders([
+                'secret' => $service['secret'],
+            ])->baseUrl($service['base_uri']);
         }
     }
 }
